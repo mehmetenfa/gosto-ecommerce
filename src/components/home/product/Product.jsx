c8888b5f607ea216e665b414b49b9753d85c0251
qxan <mehmet.enfa@gmail.com>
2023-04-02 23:25:26 +0300

    product

--- src/components/home/product/Product.jsx
+++ src/components/home/product/Product.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { useState } from "react";
+import { products } from "../../assets/data/data";
+import { Heading } from "../../common/Heading";
+import { ProductItems } from "./ProductItems";
 
 export const Product = () => {
+  const [cartItems, setCartItems] = useState(products);
   return (
-    <div>Product</div>
-  )
-}
+    <>
+      <section className="product">
+        <div className="container">
+          <Heading
+            title="Trendings Products"
+            desc="Check the hottest designs of the week. These rising stars are worth your attention."
+          />
+
+          <ProductItems cartItems={cartItems} />
+        </div>
+      </section>
+    </>
+  );
+};

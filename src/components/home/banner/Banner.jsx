d1e292181adc7fe3c82719c2d661c67c878dfd0b
qxan <mehmet.enfa@gmail.com>
2023-04-02 23:55:07 +0300

    banner

--- src/components/home/banner/Banner.jsx
+++ src/components/home/banner/Banner.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { banner } from "../../assets/data/data";
 
 export const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <>
+      <section className="banner">
+        <div className="posts">
+          {banner.map((items) => (
+            <div className="post">
+              <div className="content" key={items.id}>
+                <div className="img">
+                  <img src={items.cover} alt="" />
+                </div>
+                <div className="text">
+                  <h2>{items.title}</h2>
+                  <h2>{items.title2}</h2>
+                  <p>{items.desc}</p>
+                  <button className="button">SHOP NOW</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};

7be932a5bd0fb12c086da2ce3f6cee5eb79b4231
qxan <mehmet.enfa@gmail.com>
2023-04-02 23:21:14 +0300

    serachItem

--- src/components/home/Hero/SearchItem.jsx
+++ src/components/home/Hero/SearchItem.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React from "react";
+import { FiShoppingBag, FiSearch } from "react-icons/fi";
+import { AiOutlineHeart } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { ADD } from "../../../controller/action";
 
-export const SearchItem = () => {
+// all copy past from productItem page
+
+export const SearchItem = ({ products, value, onSearch }) => {
+  const dispatch = useDispatch();
+  const addToCart = (e) => {
+    dispatch(ADD(e));
+  };
+
   return (
-    <div>SearchItem</div>
-  )
-}
+    <>
+      <section className="searchItems">
+        <div className="product_items">
+          {products
+            .filter((items) => {
+              const searchkey = value.toLowerCase();
+              const title = items.title.toLowerCase();
+
+              return (
+                searchkey && title.startWith(searchkey) && title !== searchkey
+              );
+            })
+            .slice(0, 10)
+            .map((items) => (
+              <div
+                className="box"
+                onClick={() => onSearch(items.title)}
+                key={items.id}
+              >
+                <div className="img">
+                  <img src={items.cover} alt="" />
+                  <div className="overlay">
+                    <button className="button" onClick={() => addToCart(items)}>
+                      <FiShoppingBag />
+                    </button>
+                    <button className="button">
+                      <AiOutlineHeart />
+                    </button>
+                    <button className="button">
+                      <FiSearch />
+                    </button>
+                  </div>
+                </div>
+                <div className="details">
+                  <h3>{items.title}</h3>
+                  <p>{items.author}</p>
+                  <h4>${items.price}</h4>
+                </div>
+              </div>
+            ))}
+        </div>
+      </section>
+    </>
+  );
+};

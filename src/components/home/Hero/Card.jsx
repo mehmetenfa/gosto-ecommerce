68d93183000386d9df5650112f6054f078e6a201
qxan <mehmet.enfa@gmail.com>
2023-04-03 14:24:06 +0300

    details

--- src/components/home/Hero/Card.jsx
+++ src/components/home/Hero/Card.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { hero } from '../../assets/data/data'
- 
+import React from "react";
+import { hero } from "../../assets/data/data";
+
 export const Card = () => {
   return (
     <>
-      <section className='cards'>
+      <section className="cards">
         {hero.map((item) => (
-          <div className='card' key={item.id}>
-            <div className='left'>
+          <div className="card" key={item.id}>
+            <div className="left">
               <img src={item.cover} alt="" />
             </div>
-            <div className='right'>
+            <div className="right">
               <h4>{item.name}</h4>
               <p>{item.items} items</p>
             </div>
@@ -18,5 +18,5 @@ export const Card = () => {
         ))}
       </section>
     </>
-  )
-}
+  );
+};
